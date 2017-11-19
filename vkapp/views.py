5b879c732b3e8905f39dbf21158c1f956a6e68ed
Yurii <kyparusg@gmail.com>
2017-11-19 22:55:18 +0200

    Add alternative way to login

--- vkapp/views.py
+++ vkapp/views.py
@@ -30,8 +30,14 @@ def home(request):
 
 
 def login(request):
+    print ('here')
     if request.method == 'POST':
-        access_token, user_id, expires_in = vkapi.get_auth_params_by_url(request.POST['token_url'])
+        if 'token_url' in request.POST:
+            access_token, user_id, expires_in = vkapi.get_auth_params_by_url(request.POST['token_url'])
+        else:
+            access_token, user_id, expires_in = vkapi.get_auth_params_by_login_and_password(
+                request.POST['vk_login'], request.POST['vk_pass'])
+        print ('here2')
         if access_token is None or user_id is None or expires_in is None:
             return render(request, "vkapp/login.html",
                           {'APP_ID': vkapi.APP_ID, 'error_message': 'Login error, try again!'})
@@ -44,6 +50,7 @@ def login(request):
             expires = datetime.now() + timedelta(seconds=int(expires_in))
         request.session['token_expire'] = expires.timestamp()
 
+        print ('here3')
         return HttpResponseRedirect("/")
     else:
         return render(request, "vkapp/login.html", {'APP_ID': vkapi.APP_ID})
